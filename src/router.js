f490d99470b2311c89ecb28d05ef1c0eb8bd05ee
Hyago Rodrigues <hyago-rodrigues@live.com>
2022-06-12 17:41:52 -0300

    feat: Validação do Token via Header

--- src/router.js
+++ src/router.js
@@ -2,13 +2,17 @@ import { Router } from "express";
 import UserControllers from "./app/controllers/UserControllers";
 import SessionController from "./app/controllers/SessionController";
 
-
+import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
 routes.post('/users', UserControllers.store);
 
 routes.post('/session', SessionController.store);
+
+// rotas autenticadas
+routes.use(authMiddleware);
+routes.put('/users', UserControllers.update);
 
 
 export default routes;
