8a64a6d1e8b2169be1a960a7c7ff38e455819e94
Hyago Rodrigues <hyago-rodrigues@live.com>
2022-06-13 15:26:56 -0300

    feat: Adicianando foto do usuário

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import multer from 'multer';
+import multerConfig from "./config/multer";
 import UserControllers from "./app/controllers/UserControllers";
 import SessionController from "./app/controllers/SessionController";
 
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
+const uploads = multer(multerConfig);
 
 routes.post('/users', UserControllers.store);
 
@@ -13,6 +16,11 @@ routes.post('/session', SessionController.store);
 // rotas autenticadas
 routes.use(authMiddleware)
 routes.put('/users',  UserControllers.update)
+
+// rotas de uploads
+routes.post('/files', uploads.single('file'), (req, res) => {
+    return res.json({ message: "ok" });
+})
 
 
 
